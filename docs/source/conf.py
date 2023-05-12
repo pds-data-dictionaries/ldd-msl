3a7a2d787bff6652e635503c61610e0aefae0fa2
PDSEN Ops <pds-ops@jpl.nasa.gov>
2023-05-12 12:56:24 -0700

    Apply latest LDD Doc Gen Config updates

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'MSL Namespace'
+project = 'Example Namespace'
 # copyright = overidden by CSS specified below
 author = 'NASA Planetary Data System'
 
